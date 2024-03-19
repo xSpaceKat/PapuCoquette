81feb0054ff828c6052fe330a9b9beb93514cd56
Katt <kathy.cs@live.com.mx>
2024-03-18 22:07:58 -0700

    Proyecto Inicio

---
+++ HeladeriaPresentacion_Equipo3/src/main/java/com/mycompany/heladeriapresentacion_equipo3/HeladeriaPresentacion_Equipo3.java
@@ -0,0 +1,12 @@
+package com.mycompany.heladeriapresentacion_equipo3;
+
+/**
+ *
+ * @author Katt
+ */
+public class HeladeriaPresentacion_Equipo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
