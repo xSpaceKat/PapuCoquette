94c6cd9e339ed6f9797d1fa93b8391cf13af9399
Nube28 <159978169+Nube28@users.noreply.github.com>
2024-05-11 15:05:24 -0700

    sera acaso que termine?
    No me faltan validaciones

--- HeladeriaNegocio/src/main/java/negocio/AgregarProductoBO.java
+++ HeladeriaNegocio/src/main/java/negocio/AgregarProductoBO.java
@@ -6,6 +6,10 @@ package negocio;
 
 import dto.ProductoDTO;
 import interfacesNegocio.IAgregarProductoBO;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.dao.ProductoDAO;
+import persistencia.excepciones.PersistenciaException;
 
 /**
  *
@@ -15,7 +19,14 @@ public class AgregarProductoBO implements IAgregarProductoBO{
 
     @Override
     public void AgregarProducto(ProductoDTO productoDTO) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Conversor conversor = new Conversor();
+        ProductoDAO productoDAO = new ProductoDAO();
+        try {
+            productoDAO.guardarProducto(conversor.DTOaDAO(productoDTO));
+        } catch (PersistenciaException ex) {
+            Logger.getLogger(AgregarProductoBO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
